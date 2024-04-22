62c3ad92751096403667b3ca9b814a1344848bd6
tafhdytllah <taufikhh.97@gmail.com>
2024-04-22 07:21:38 +0700

    update database.sql query

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
--- Active: 1713612460274@@127.0.0.1@5432@psql_db
-DROP DATABASE IF EXISTS psql_db;
+-- Active: 1713744276875@@127.0.0.1@5432@psql_db
+-- DROP DATABASE IF EXISTS psql_db;
 
 CREATE DATABASE psql_db;
 
-\c psql_db;
+-- \c psql_db;
 
 CREATE TABLE nationality(
     nationality_id SERIAL PRIMARY KEY,
@@ -12,20 +12,30 @@ CREATE TABLE nationality(
 );
 
 INSERT INTO nationality(nationality_name, nationality_code)
-VALUES ('Indonesia', 'ID');
+VALUES 
+    ('Indonesia', 'ID'), 
+    ('Malaysia', 'MY'), 
+    ('Singapore', 'SG'), 
+    ('Saudi Arabia', 'SA'), 
+    ('United States', 'US');
+
 
 CREATE TABLE customer(
     cst_id SERIAL PRIMARY KEY,
     nationality_id INT NOT NULL,
-    cst_name CHAR(50) NOT NULL,
+    cst_name VARCHAR(50) NOT NULL,
     cst_dob DATE NOT NULL,
     cst_phonenum VARCHAR(20) NOT NULL,
-    cst_email varchar(50) NOT NULL,
+    cst_email VARCHAR(50) NOT NULL,
     CONSTRAINT fk_nationality FOREIGN KEY(nationality_id) REFERENCES nationality(nationality_id)
 );
 
 INSERT INTO customer(nationality_id, cst_name, cst_dob, cst_phonenum, cst_email)
-VALUES (1, 'joko', '1999-12-23', '62813829728', 'joko@gmail.com')
+VALUES 
+(1, 'joko', '1992-05-19', '62812345678', 'joko@gmail.com'),
+(3, 'budi', '1998-03-28', '62898765342', 'budi@gmail.com'),
+(5, 'sinta', '1990-12-01', '62818726261', 'sinta@gmail.com');
+
 
 CREATE TABLE family_list(
     fl_id SERIAL PRIMARY KEY,
@@ -37,4 +47,11 @@ CREATE TABLE family_list(
 );
 
 INSERT INTO family_list(cst_id, fl_relation, fl_name, fl_dob)
-VALUES (1, 'ayah', 'budi', '1970-4-16')
+VALUES
+(1, 'ayah', 'tono', '1950-11-20'),
+(2, 'ayah', 'mustopo', '1975-08-06'),
+(2, 'ibu', 'sumiati', '1980-01-14'),
+(1, 'ibu', 'ratih', '1952-09-16'),
+(1, 'adik', 'tora', '2000-06-28'),
+(3, 'kakak', 'trisna', '1990-03-27'),
+(1, 'kakak', 'siska', '1970-08-09');
